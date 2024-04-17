feecc24303373d561139f227ce3353e78c105c45
Prernamhaisane <91752549+Prernamhaisane@users.noreply.github.com>
2024-04-17 15:27:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from tensorflow.keras.preprocessing import image
 filename = r"C:\Users\PRERNA\OneDrive\Desktop\B.TechProject\riceleafdisease1.h5"
     # Open the HDF5 file
     with h5py.File(filename, 'r') as f:
-        # Load the model
         loaded_model = tf.keras.models.load_model(filename)
 
 
